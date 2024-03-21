b0b44d7bb4affb35294c66e1a5f3e37bc706faa9
Annie <93747286+anakolchina97@users.noreply.github.com>
2024-03-21 21:39:52 +0300

    Fix

--- src/includes/parallax/parallax.js
+++ src/includes/parallax/parallax.js
@@ -5,42 +5,19 @@ gsap.registerPlugin(ScrollTrigger);
 class Parallax {
 	constructor() {
 		this.planets = document.querySelectorAll('.parallax__planet');
-		this.scaleDownValue = 1.2; // Масштаб для прокрутки вниз
-		this.animationActive = false; // Ф
-
 		this.init();
 	}
 	init() {
-		window.addEventListener('load', function () {
-			console.log(1);
-			// После загрузки страницы устанавливаем флаг в true
-			this.animationActive = true;
-		});
 		this.planets.forEach(planet => {
 			const depth = parseFloat(planet.getAttribute('data-depth'));
 
 			gsap.to(planet, {
-				scale: 1,
-				y: 10 * depth,
 				ease: 'none',
+				scale: 2,
+				y: 10 * depth,
 				scrollTrigger: {
-					trigger: planet,
-					start: 'center bottom',
-					end: 'center top',
-					scrub: true,
 					markers: true,
-					onUpdate: self => {
-						// Проверяем, активна ли анимация
-						if (this.animationActive) {
-							// Применяем анимацию только при прокрутке вниз
-							if (self.direction === 1) {
-								console.log(1);
-								gsap.to(planet, { scale: this.scaleDownValue });
-							} else {
-								gsap.to(planet, { scale: 1 });
-							}
-						}
-					},
+					scrub: true,
 				},
 			});
 		});
